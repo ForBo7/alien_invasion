81af3961f17c536684e1ff96401f7cba82ae5c5c
ForzBox7 <salmananaqvii+github@gmail.com>
2021-07-08 17:35:43 +0500

    The game now fullscreens automatically when opened.

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,8 +13,9 @@ class AlienInvasion:
         """Initialize the game, and create game resources."""
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_width,
-                                               self.settings.screen_height,))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         self.ship = Ship(self)
 
         pygame.display.set_caption("Alien Invasion")
