6c53fdb6ec2c04d7527cb274166ee51dde6daee5
ForzBox7 <salmananaqvii+github@gmail.com>
2021-07-11 21:23:17 +0500

    A single row of aliens now instantiates at the top of the screen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -79,9 +79,21 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
 
     def _create_fleet(self):
-        """Create a fleet of aliens."""
-        new_alien = Alien(self)
-        self.aliens.add(new_alien)
+        """Create the fleet of aliens."""
+        # Create an alien and find the number of aliens in a row.
+        #  Spacing between each alien is equal to alien width.
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # Create the first row of aliens.
+        for alien_number in range(number_aliens_x):
+            # Create an alien and place it in the row.
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
