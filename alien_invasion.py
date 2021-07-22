19bddc4fd01485d5aa15944aace318c6bdb09a18
ForzBox7 <salmananaqvii+github@gmail.com>
2021-07-22 12:52:26 +0500

    The previously mentioned bug where the entire fleet of aliens goes off the screen after a few are destroyed is now fixed.

--- alien_invasion.py
+++ alien_invasion.py
@@ -87,7 +87,7 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens,
                                                 True, True)
         if not self.aliens:
-            # Destroy existing bullets and cerate new fleet.
+            # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
 
@@ -122,18 +122,18 @@ class AlienInvasion:
         alien.rect.y = alien.y
         self.aliens.add(alien)
 
-
     def _check_fleet_edges(self):
         """Respond appropriately if any aliens have reached an edge."""
         for alien in self.aliens.sprites():
             if alien.check_edges():
                 self._change_fleet_direction()
+                break
 
     def _change_fleet_direction(self):
         """Drop the entire fleet and change the fleet's direction."""
         for alien in self.aliens.sprites():
             alien.rect.y += self.settings.fleet_drop_speed
-            self.settings.fleet_direction *= -1
+        self.settings.fleet_direction *= -1
 
     def _update_aliens(self):
         """
